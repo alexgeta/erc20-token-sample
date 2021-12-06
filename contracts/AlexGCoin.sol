f3c91327363c9e7f28e343458852e8cb0d4ba1a5
alexgeta <freedfromdesire21@gmail.com>
2021-12-06 17:07:29 +0200

    Implement all ERC20 functions

--- contracts/AlexGCoin.sol
+++ contracts/AlexGCoin.sol
@@ -6,8 +6,12 @@ interface ERC20 {
     function totalSupply() external view returns (uint);
     function balanceOf(address tokenOwner) external view returns (uint balance);
     function transfer(address to, uint tokens) external returns (bool success);
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint tokens) external returns (bool success);
 
     event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
 
 contract SafeMath {
@@ -28,8 +32,10 @@ contract AlexGCoin is ERC20, SafeMath {
 
     string public name = "AlexGCoin";
     string public symbol = "AGC";
+    uint8 public decimals = 18;
     uint256 public _totalSupply = 10 ** 13;
     mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
 
     constructor() ERC20() {
         balances[msg.sender] = _totalSupply;
@@ -48,6 +54,24 @@ contract AlexGCoin is ERC20, SafeMath {
         balances[msg.sender] = safeSub(balances[msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][to] = safeSub(allowed[from][to], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view returns (uint remaining) {
+        return allowed[owner][spender];
+    }
+
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
         return true;
     }
 
